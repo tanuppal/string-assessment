473f4c5f429cd3536e752285c7f96e20412884c4
tanup pal <pal1993tanup@gmail.com>
2024-06-06 18:37:08 +0530

    Handle Two Numbers and update test file

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
+
+    numbers.split(",").map(&:to_i).reduce(:+)
   end
 end

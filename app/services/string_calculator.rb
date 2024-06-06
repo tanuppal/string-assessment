420f1412269c6317c1151d4e92be3e5088bc7836
tanup pal <pal1993tanup@gmail.com>
2024-06-06 19:01:38 +0530

    Support Different Delimiters and update test file

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -3,6 +3,13 @@ class StringCalculator
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
 
-    numbers.split(",").map(&:to_i).reduce(:+)
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers = parts[1]
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).reduce(:+)
   end
 end
